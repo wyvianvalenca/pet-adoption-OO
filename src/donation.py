9fbc4ed5c35aee7440771cc9f98fce0f958c3f68
Wyvian Valença <wgcv2@ic.ufal.br>
2025-08-22 01:05:43 -0300

    feat: donation class implemented

---
+++ src/donation.py
@@ -0,0 +1,46 @@
+from datetime import date
+from typing_extensions import override
+
+
+class Donation:
+    all: list['Donation'] = []
+
+    def __init__(self, donor: str, receiver: str, ammount: float,
+                 donation_date: date):
+        self.__donor: str = donor
+        self.__receiver: str = receiver
+        self.__ammount: float = ammount
+        self.__donation_date: date = donation_date
+
+        self.all.append(self)
+
+    @property
+    def donor(self) -> str:
+        return self.__donor
+
+    @property
+    def receiver(self) -> str:
+        return self.__receiver
+
+    @property
+    def ammount(self) -> float:
+        return self.__ammount
+
+    @property
+    def date(self) -> str:
+        return self.__donation_date.isoformat()
+
+    @override
+    def __str__(self) -> str:
+        return f"@{self.__donor} has donated US$ {self.__ammount:.2f} to @{self.__receiver}"
+
+    # overloading comparison operators
+    def __gt__(self, other):
+        if isinstance(other, Donation):
+            return self.ammount > other.ammount
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Donation):
+            return self.ammount < other.ammount
+        return NotImplemented
