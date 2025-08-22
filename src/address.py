94dbf16f2ce61120900d1ded540f7247878a5f6c
Wyvian Valença <wgcv2@ic.ufal.br>
2025-08-22 01:06:31 -0300

    refactor: encapsulation implemented in Address

--- src/address.py
+++ src/address.py
@@ -4,14 +4,14 @@ from typing_extensions import override
 class Address:
     def __init__(self, street: str, district: str, number: str,
                  postal_code: int, city: str, state: str):
-        self.street: str = street
-        self.district: str = district
-        self.number: str = number
-        self.postal_code: int = postal_code
-        self.city: str = city
-        self.state: str = state
+        self.__street: str = street
+        self.__district: str = district
+        self.__number: str = number
+        self.__postal_code: int = postal_code
+        self.__city: str = city
+        self.__state: str = state
 
     @override
     def __str__(self) -> str:
-        return (f"{self.street}, {self.number}, {self.district} - "
-                + f"{self.city}/{self.state.upper()}")
+        return (f"{self.__street}, {self.__number}, {self.__district} - "
+                + f"{self.__city}/{self.__state.upper()}")
