2de40774ea2bf15e6d49e7c7d585119b4342670f
Wyvian Valença <wgcv2@ic.ufal.br>
2025-08-27 10:36:56 -0300

    refactor: profile has None as default values for all attributes

--- src/user_profile.py
+++ src/user_profile.py
@@ -1,16 +1,18 @@
 from datetime import date
+from dateutil import relativedelta
 from typing_extensions import override
 
 from .address import Address
 
 
 class Profile:
-    def __init__(self, name: str, birth: date, address: Address,
-                 description: str):
+    def __init__(self, name: str, birth: date | None = None,
+                 address: Address | None = None,
+                 desc: str | None = None):
         self.__name: str = name
-        self.__birth: date = birth
-        self.__address: Address = address
-        self.__description: str = description
+        self.__birth: date | None = birth
+        self.__address: Address | None = address
+        self.__description: str | None = desc
 
     @property
     def name(self) -> str:
@@ -19,14 +21,34 @@ class Profile:
     # TODO: add properties and setters with data validation
 
     def as_list(self) -> list[str]:
-        return [self.__name, self.__birth.isoformat(),
-                self.__address.__str__(), self.__description]
+        info: list[str] = [self.__name]
+
+        if self.__birth:
+            info.append(self.__birth.isoformat())
+
+        if self.__address:
+            info.append(self.__address.__str__())
+
+        if self.__description:
+            info.append(self.__description)
+
+        return info
 
     def formatted_list(self) -> list[str]:
-        profile_info: list[str] = [f"{self.__name.upper()}",
-                                   f"    > Description: {self.__description}",
-                                   f"    > Idade: {self.__birth.today()}",
-                                   f"    > Addres: {self.__address}"]
+        profile_info: list[str] = [f"{self.__name.upper()}"]
+
+        if self.__birth:
+            today = date.today()
+            age: int = relativedelta.relativedelta(today, self.__birth).years
+            profile_info.append(f"    > [bold]Age:[/] {age}")
+
+        if self.__address:
+            profile_info.append(f"    > [bold]Address:[/] {self.__address}")
+
+        if self.__description:
+            profile_info.append(
+                f"    > [bold]Description:[/] {self.__description}")
+
         return profile_info
 
     @override
